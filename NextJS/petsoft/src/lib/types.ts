77effeca51d0c70dffe01a898c388e0f3392669d
Aiden <ateal@ualberta.ca>
2024-08-21 21:10:53 -0600

    petsoft project

--- NextJS/petsoft/src/lib/types.ts
+++ NextJS/petsoft/src/lib/types.ts
@@ -2,4 +2,4 @@ import { Pet } from "@prisma/client";
 
 export type TPetEssentials = Omit<Pet, "id" | "createdAt" | "updatedAt" | "userId">;
 
-export type TPet = Omit<Pet, "createdAt" | "updatedAt">
+export type TPet = Omit<Pet, "createdAt" | "updatedAt" | "userId">
